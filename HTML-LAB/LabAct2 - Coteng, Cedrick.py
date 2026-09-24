e06ad235d9216c4257c904b6712d03302536d77e
jehmarcongostudent-alt <jehmarcongo.student@gmail.com>
2026-09-24 08:54:54 +0800

    Add files via upload

---
+++ HTML-LAB/LabAct2 - Coteng, Cedrick.py
@@ -0,0 +1,56 @@
+lbs = 50
+kg = lbs * 0.453592
+
+print("Pounds:", lbs)
+print("Kilograms:", kg)
+
+# Miles to Kilometers
+mi = 10
+km = mi * 1.60934
+
+print("Miles:", mi)
+print("Kilometers:", km)
+
+# Fahrenheit to Celsius
+fahrenheit = 86
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("Fahrenheit:", fahrenheit)
+print("Celsius:", celsius)
+
+# Average age of 10 students
+age1 = 15
+age2 = 16
+age3 = 15
+age4 = 17
+age5 = 16
+age6 = 15
+age7 = 16
+age8 = 17
+age9 = 15
+age10 = 16
+
+average = (age1 + age2 + age3 + age4 + age5 + age6 + age7 + age8 + age9 + age10) / 10
+
+print("\nAges of the students:")
+print(age1, age2, age3, age4, age5)
+print(age6, age7, age8, age9, age10)
+print("Average age:", average)
+
+# Fantasy story
+hero = "Luna"
+sword = "Moon Sword"
+dragon = "Ember"
+potion = "Healing Potion"
+spell = "Fireball"
+
+story = """
+Once upon a time, """ + hero + """ went into a dark forest.
+She carried the """ + sword + """ and met a dragon named """ + dragon + """.
+The dragon was angry, so Luna drank a """ + potion + """.
+Then she used the """ + spell + """ and defeated the dragon.
+Luna became a hero and lived happily ever after.
+"""
+
+print("\nFantasy Story:")
+print(story)
---
+++ HTML-LAB/Untitled-1.py
@@ -0,0 +1,5 @@
+print(" age = 12")
+student_age = 12
+print(f"Student Age is {student_age}")
+age = 12
+print(age)
